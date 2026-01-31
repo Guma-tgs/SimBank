0286088e3c3145f518996c48b5f834d8fd5fb667
Guma-tgs <gulmiratuleberdieva@gmail.com>
2026-01-31 15:45:21 +0600

    January promoution Gulmira.
    January promoution
    
    January Promoution2
    
    January Promoution3
    
    January Promoution4
    
    January Promoution5

---
+++ src/kg/sim_bank/Promoution/JanuaryPromoution.java
@@ -0,0 +1,15 @@
+package kg.sim_bank.Promoution;
+
+public class JanuaryPromoution {
+    public static void main(String[] args) {
+
+        System.out.println("January");
+        System.out.println("January123");
+        System.out.println("January11234");
+        System.out.println("January11245567");
+        System.out.println("January11248475S");
+        System.out.println("January11248475S1");
+        System.out.println("January11248475S12");
+        System.out.println("January11248475S123");
+    }
+}
