b3e33912d4424efc16b35b60bda04ee9d0b66d49
Guma-tgs <gulmiratuleberdieva@gmail.com>
2026-02-01 20:17:40 +0600

    Reset1

---
+++ src/revertAndReset/ResetDemo1.java
@@ -0,0 +1,9 @@
+package revertAndReset;
+
+public class ResetDemo1 {
+    public static void main(String[] args) {
+
+        System.out.println("Reset one");
+        System.out.println("eroor234git");
+    }
+}
